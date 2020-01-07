0e38cde6f773fc7a731a63ae97f8883540a9fd2c
Andreas Wagner <awagner@gmx.de>
2020-01-07 16:23:17 +0100

    MBS-4222: compile (minify) amd module

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2019120300;           // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2020010700;           // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2016052301;           // Requires this Moodle version.
 $plugin->component = 'filter_mbsembed';    // Full name of the plugin (used for diagnostics).
 $plugin->maturity  = MATURITY_ALPHA;
